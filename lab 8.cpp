089aca0848970c381eddd4576fbb8822c6228087
muddasar782 <muddasarwaheed25@gmail.com>
2025-05-26 23:54:34 +0500

    Add files via upload

---
+++ lab 8.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+// Node template
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(T val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+// Binary Tree template
+template <typename T>
+class BinaryTree {
+private:
+    TreeNode<T>* root;
+
+    // Recursive insert
+    TreeNode<T>* insert(TreeNode<T>* node, T val) {
+        if (!node) return new TreeNode<T>(val);
+        if (val < node->data)
+            node->left = insert(node->left, val);
+        else
+            node->right = insert(node->right, val);
+        return node;
+    }
+
+    // Inorder traversal (Left, Root, Right)
+    void inorder(TreeNode<T>* node) {
+        if (!node) return;
+        inorder(node->left);
+        cout << node->data << " ";
+        inorder(node->right);
+    }
+
+    // Preorder traversal (Root, Left, Right)
+    void preorder(TreeNode<T>* node) {
+        if (!node) return;
+        cout << node->data << " ";
+        preorder(node->left);
+        preorder(node->right);
+    }
+
+    // Postorder traversal (Left, Right, Root)
+    void postorder(TreeNode<T>* node) {
+        if (!node) return;
+        postorder(node->left);
+        postorder(node->right);
+        cout << node->data << " ";
+    }
+
+    // Destructor helper
+    void destroy(TreeNode<T>* node) {
+        if (!node) return;
+        destroy(node->left);
+        destroy(node->right);
+        delete node;
+    }
+
+public:
+    BinaryTree() : root(nullptr) {}
+    ~BinaryTree() { destroy(root); }
+
+    void insert(T val) {
+        root = insert(root, val);
+    }
+
+    void inorder() {
+        cout << "Inorder traversal: ";
+        inorder(root);
+        cout << endl;
+    }
+
+    void preorder() {
+        cout << "Preorder traversal: ";
+        preorder(root);
+        cout << endl;
+    }
+
+    void postorder() {
+        cout << "Postorder traversal: ";
+        postorder(root);
+        cout << endl;
+    }
+};
+
+int main() {
+    BinaryTree<int> bt;
+    
+    // Insert some values
+    bt.insert(5);
+    bt.insert(3);
+    bt.insert(7);
+    bt.insert(2);
+    bt.insert(4);
+    bt.insert(6);
+    bt.insert(8);
+    
+    // Perform traversals
+    bt.inorder();
+    bt.preorder();
+    bt.postorder();
+    
+    return 0;
+}
